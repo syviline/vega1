f14a99c11d870982574edcb6508d0050e24e394b
syvlline <dovase71@gmail.com>
2021-03-11 20:07:32 +0500

    task template

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('landing.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/task')
+def task():
+    return render_template('task.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
